8ab0e6bc9f5524c7643a40f1cc61e65f9959f53a
dboures <dboures@att.net>
2020-11-13 12:40:00 -0600

    begin mouse click

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ import pygame as pg
 #Inititalize the pygame
 pg.init()
 
-#Title and Icon
-pg.display.set_caption("Hive")
-icon = pg.image.load('icon.png')
-pg.display.set_icon(icon)
-
 #Create the screen
 screen = pg.display.set_mode((800,600))
+
+#Title and Icon
+pg.display.set_caption("Hive")
+#TODO: Icon not working on Ubuntu??
+icon = pg.image.load('icon.png')
+pg.display.set_icon(icon)
 
 running = True
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if pg.mouse.get_pressed()[0]:
+                pos = pg.mouse.get_pos()
+                #TODO: create hex and get position to print what hex we are in
+                #Use axial coords
+                print(pos)
+
+    screen.fill((0,0,0))
+    pg.display.update()
