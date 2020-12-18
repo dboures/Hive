331ecb7f3646d14995043922b3b26dddc1da1969
D BO <dboures@att.net>
2020-12-18 12:53:47 -0600

    can handle multiple games

--- server.py
+++ server.py
@@ -20,13 +20,6 @@ WIDTH, HEIGHT = 880, 900
 s =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host =  "192.168.1.30"
 port = 5555
-current_player  = 0
-
-init_state = Game_State(initialize_grid(HEIGHT - 200, WIDTH, radius=20))
-
-init_state.start_game()
-
-games = [init_state]
 
 try:
     s.bind((host,port))
@@ -36,7 +29,11 @@ except socket.error as e:
 print('Waiting for a connection .. ')
 s.listen(2)
 
-def threaded_client(conn, current_player):
+games = {}
+idCount = 0
+
+def threaded_client(conn, current_player, gameId):
+    global idCount
     conn.send(pickle.dumps(current_player)) #not sure what should be sent here
     while True:
         try:
@@ -48,19 +45,19 @@ def threaded_client(conn, current_player):
                 pass
             elif data == "get_state":
                 print('get_state')
-                reply = games[0]
+                reply = games[gameId]
                 conn.sendall(pickle.dumps(reply))
                 print('Sent reply')
 
             elif data == "get_turn_and_board":
                 # print('get_board')
-                reply = (games[0].turn ,games[0].board_tiles)
+                reply = (games[gameId].turn ,games[gameId].board_tiles)
                 conn.sendall(pickle.dumps(reply))
                 # print('Sent reply')
 
             elif type(data) is Game_State:
                 print('Received game state')
-                games[0] = data
+                games[gameId] = data
                 reply = data
                 conn.sendall(pickle.dumps(reply))
                 print('Sent reply')
@@ -76,7 +73,7 @@ def threaded_client(conn, current_player):
                 #         if type(piece) is Queen and tile.coords == (440, 380):
                 #             print('Queen in middle')
 
-                games[0].board_tiles = data
+                games[gameId].board_tiles = data
                 reply = data
                 conn.sendall(pickle.dumps(reply))
                 print('Sent reply')
@@ -85,23 +82,23 @@ def threaded_client(conn, current_player):
                 print('Received move')
                 reply = False
                 playerid = data.player
-                games[0].add_moving_piece(data.piece)
+                games[gameId].add_moving_piece(data.piece)
                 old_coords = data.old_coords
                 new_coords = data.new_coords
                 print(playerid, old_coords, new_coords)
 
-                if games[0].player_can_move(playerid):
+                if games[gameId].player_can_move(playerid):
                     print(str(playerid) + ' can move')
                     old_tile = next(
-                        (tile for tile in games[0].board_tiles if tile.coords == old_coords), None)
+                        (tile for tile in games[gameId].board_tiles if tile.coords == old_coords), None)
                     new_tile = next(
-                        (tile for tile in games[0].board_tiles if tile.coords == new_coords), None)
+                        (tile for tile in games[gameId].board_tiles if tile.coords == new_coords), None)
                     print(old_tile, new_tile)
                     #send old_tile, new_tile
-                    if is_valid_move(games[0], old_tile, new_tile):
+                    if is_valid_move(games[gameId], old_tile, new_tile):
                         print('move is valid')
                         old_tile.move_piece(new_tile)
-                        games[0].next_turn()
+                        games[gameId].next_turn()
                         reply = True
                         print('reply created')
                         # if player_has_no_moves(state):
@@ -109,15 +106,34 @@ def threaded_client(conn, current_player):
                         #     state.open_popup()
                 conn.sendall(pickle.dumps(reply))
                 print('Sent reply')
-                games[0].remove_moving_piece() # clunky but hope it works
+                games[gameId].remove_moving_piece() # clunky but hope it works
         except:
             break
+
+    print("Lost Connection")
+    try:
+        del games[gameId]
+        print("Closing Game", gameId)
+    except:
+        pass
+    idCount -= 1
+    conn.close()
 
 while True:
     conn, addr = s.accept()
     print('Connected to:' + addr[0] +':' + str(addr[1]))
-    start_new_thread(threaded_client, (conn,current_player % 2)) #figure out a way to group players
-    current_player += 1
-    print('Thread Number: ' + str(current_player))
+    idCount += 1
+    gameId = (idCount - 1)//2
+    if idCount % 2 == 1:
+        print("Creating a new game...")
+        new_game = Game_State(initialize_grid(HEIGHT - 200, WIDTH, radius=20))
+        new_game.start_game() # going to handle waiting here eventually
+        games[gameId] = new_game
+        start_new_thread(threaded_client, (conn, 0, gameId)) 
+    else:
+        start_new_thread(threaded_client, (conn, 1, gameId))
+        
+    print('Thread Number: ' + str(idCount))
+
 
 s.close()
